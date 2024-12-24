0d89d0bf563cfd82314057e8320d5e0a26d278ac
Pratik Singh <pratikkumars623@gmail.com>
2024-12-25 00:48:00 +0530

    project completed

--- script.js
+++ script.js
@@ -194,3 +194,48 @@ generateBtn.addEventListener('click',()=>{
     calcStrength();
 
 })
+
+
+
+// const inputSlider=document.querySelector("[data-lengthSlide]");   //using query selector we are fetching attributes
+// const lengthDisplay=document.querySelector("[data-lengthNumber]");
+// const passwordDisplay = document.querySelector("[data-passwordDisplay]");
+// const copyBtn = document.querySelector("[data-copy]");
+// const copyMsg = document.querySelector("[data-copyMsg]");
+// const uppercaseCheck = document.querySelector("#uppercase");
+// const lowercaseCheck = document.querySelector("#lowercase");
+// const numbersCheck = document.querySelector("#numbers");
+// const symbolsCheck = document.querySelector("#symbols");
+// const indicator = document.querySelector("[data-indicator]");
+// const generateBtn = document.querySelector(".generateButton");
+// const allCheckBox = document.querySelectorAll("input[type=checkbox]");
+// const symbols = '~`!@#$%^&*()_-+={[}]|:;"<,>.?/';
+
+// const inputSlider=document.querySelector("[data-lengthSlide]");   //using query selector we are fetching attributes
+// const lengthDisplay=document.querySelector("[data-lengthNumber]");
+// const passwordDisplay = document.querySelector("[data-passwordDisplay]");
+// const copyBtn = document.querySelector("[data-copy]");
+// const copyMsg = document.querySelector("[data-copyMsg]");
+// const uppercaseCheck = document.querySelector("#uppercase");
+// const lowercaseCheck = document.querySelector("#lowercase");
+// const numbersCheck = document.querySelector("#numbers");
+// const symbolsCheck = document.querySelector("#symbols");
+// const indicator = document.querySelector("[data-indicator]");
+// const generateBtn = document.querySelector(".generateButton");
+// const allCheckBox = document.querySelectorAll("input[type=checkbox]");
+// const symbols = '~`!@#$%^&*()_-+={[}]|:;"<,>.?/';
+
+// const inputSlider=document.querySelector("[data-lengthSlide]");   //using query selector we are fetching attributes
+// const lengthDisplay=document.querySelector("[data-lengthNumber]");
+// const passwordDisplay = document.querySelector("[data-passwordDisplay]");
+// const copyBtn = document.querySelector("[data-copy]");
+// const copyMsg = document.querySelector("[data-copyMsg]");
+// const uppercaseCheck = document.querySelector("#uppercase");
+// const lowercaseCheck = document.querySelector("#lowercase");
+// const numbersCheck = document.querySelector("#numbers");
+// const symbolsCheck = document.querySelector("#symbols");
+// const indicator = document.querySelector("[data-indicator]");
+// const generateBtn = document.querySelector(".generateButton");
+// const allCheckBox = document.querySelectorAll("input[type=checkbox]");
+// const symbols = '~`!@#$%^&*()_-+={[}]|:;"<,>.?/';
+
